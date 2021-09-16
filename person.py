7fd683b73904b250d40dacce693caec8871a0e4f
Sam Fortuin <99059050@mydavinci.nl>
2021-09-16 13:40:23 +0200

    commit1

---
+++ person.py
@@ -0,0 +1,10 @@
+#99059050, Sam Fortuin
+#var
+name = input("What is your name? ")
+adress = input("What's your adress? ")
+postalCode = input("What is your postal code? ")
+city = input("Which city do you live in? ")
+
+#print in single statement for efficiency
+#spaces for scuffed text line up
+print("--------------------------------------------------\n"+"| Name:          "+name+"\n| Adress:        "+adress+"\n| Postal Code:   "+postalCode+"\n| City:          "+city+"\n--------------------------------------------------")
