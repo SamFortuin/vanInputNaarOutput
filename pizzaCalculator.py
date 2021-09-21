deeda0e68acfa12f6f3767825934993df0014c13
Sam Fortuin <99059050@mydavinci.nl>
2021-09-21 14:17:05 +0200

    added subprices per pizza catogory

--- pizzaCalculator.py
+++ pizzaCalculator.py
@@ -1,22 +1,27 @@
 #99059050, Sam Fortuin, pizzaCalculator
+import time
 
 def main():
     #vars
-    smallPizzaAmount = float(input("How many small pizza's do you want?\n"))
-    mediumPizzaAmount = float(input("How many medium pizza's do you want?\n"))
-    largePizzaAmount = float(input("How many large pizza's do you want?\n"))
+    smallPizzaAmount = int(input("How many small pizza's do you want?\n"))
+    mediumPizzaAmount = int(input("How many medium pizza's do you want?\n"))
+    largePizzaAmount = int(input("How many large pizza's do you want?\n"))
     #prices in €, based on NYPizza
     pizzaPrices = [11.99, 13.99, 16.99]
     smallPizzaPrice, mediumPizzaPrice, largePizzaPrice = pizzaPrices
 
-    #sum for price
-    totalPrice = (smallPizzaAmount * smallPizzaPrice) + (mediumPizzaAmount * mediumPizzaPrice) + (largePizzaAmount * largePizzaPrice)
+    #sum for prices
+    smallPizzaTotal = smallPizzaAmount * smallPizzaPrice
+    mediumPizzaTotal = mediumPizzaAmount * mediumPizzaPrice
+    largePizzaTotal = largePizzaAmount * largePizzaPrice
+    totalPrice = smallPizzaTotal + mediumPizzaTotal + largePizzaTotal
     
     #print final calculator line
-    print("Your order of",int(smallPizzaAmount),"small pizza's,",int(mediumPizzaAmount),"medium pizza's, and",int(largePizzaAmount),"large pizza's costs €"+str(round(totalPrice,2)),"in total")
+    print("\n\n--------------------\nYour order:\n--------------------\n\n"+str(smallPizzaAmount),"small pizza's totaling €"+str(round(smallPizzaTotal,2))+"\n"+str(mediumPizzaAmount),"medium pizza's totaling €"+str(round(mediumPizzaTotal,2))+"\n"+str(largePizzaAmount),"large pizza's totaling €"+str(round(largePizzaTotal,2)),"\n\n--------------------\nOrder Total: €"+str(round(totalPrice,2))+"\n--------------------\n")
     
-    #asks if another order & lowers it
-    restart = input("Do you want to buy more pizza's? Y/N\n")
+    #asks if another order after 500ms & lowers it
+    time.sleep(0.5)
+    restart = input("Do you want to buy more pizza's? Y/N ")
     restart = restart.lower()
 
     #if yes is typed program restarts itself
@@ -24,10 +29,14 @@ def main():
         main()
     #if no is typed exits with ty msg
     elif restart == "n" or restart == "no":
-        print("thank you for using the calculator. program will now exit")
+        print("Thank you for using the calculator. program will now exit")
+        time.sleep(0.2)
         exit()
     else:
-        print("input not regonized as any form of yes or no. program will now exit")
+        print("Input not regonized as any form of yes or no. program will now exit")
+        time.sleep(0.2)
         exit()
 
 main()
+
+#add name for the order
